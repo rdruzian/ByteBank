7dbd0c22cc4d22c83c3d13736392efda41ff076b
rdruzian <rendruzian@gmail.com>
2021-07-07 10:48:45 -0300

    Entendo e aplicando exceções

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,7 +10,56 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Metodo();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Stack de erro");
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine("Aconteceu um erro!");
+            }
+
             Console.ReadLine();
+        }
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(2);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                int resultado = Dividir(10, divisor);
+
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Mensagem de erro interna do .Net");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Stack de erro");
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine("Não é possível fazer uma divisão por 0!");
+            }
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor = " + divisor);
+                // throw retorna a exceção para frente, indicando que houve um exceção, não sendo necessário criar código de erro
+                throw;
+            }
         }
     }
 }
